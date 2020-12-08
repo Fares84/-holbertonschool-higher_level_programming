67c97878786a5e81f7d679934815d9b3397d47d9
Fares84 <fares.sassi2015@gmail.com>
2020-12-08 22:58:38 +0000

    2 digit with cond

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(0, 90):
     if number == 89:
-        print ("{:d}".format(number))
+        print("{:d}".format(number))
     else:
-        number / 10 < number % 10
-        print("{:02d},".format(number), end=' ')
+        (number / 10) < (number % 10)
+        print("{:02d}".format(number), end=', ')
