8d36d332a0c8c29a1fc99ff8ef0e9929ee7d9d35
Fares84 <fares.sassi2015@gmail.com>
2020-12-09 09:06:43 +0000

    two digit condition

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for number in range(0, 90):
-    if number == 89:
-        print("{:d}".format(number))
+for nb in range(0, 90):
+    if nb == 89:
+        print("{:d}".format(nb))
     else:
-        (number / 10) < (number % 10)
-        print("{:02d}".format(number), end=', ')
+        if nb / 10 < nb % 10:
+            print("{:02d}".format(nb), end=', ')
