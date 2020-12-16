c598467022042a2a5014aec5d2ea670a140eda6e
Fares84 <fares.sassi2015@gmail.com>
2020-12-16 13:21:44 +0000

    updated file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
+   import hidden_4
    for i in range(len(dir(hidden_4))):
        if dir(hidden_4)[i][0] != '_':
             print("{}".format(dir(hidden_4)[i]))
