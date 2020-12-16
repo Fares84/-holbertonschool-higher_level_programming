c2c3bfb1e3a2ee28586cf3565c1db33d127b2466
Fares84 <fares.sassi2015@gmail.com>
2020-12-16 19:43:57 +0000

    extra time

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+if __name__ == "__main__":
+     if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+     operator = ["+", "-", "*", "/"]
+     if argv[2] not in operator:
+         print("Unknown operator. Available operators: +, -, * and /")
+         exit(1)
+
+     a = int(argv[1])
+     b = int(argv[3])
+
+     if argv[2] == "+":
+         res = add(a, b)
+     elif argv[2] == "-":
+         res = sub(a, b)
+     elif argv[2] == "*":
+         res = mul(a, b)
+     else:
+         res = div(a, b)
+
+     print("{} {} {} = {}".format(a, argv[2], b, res))
