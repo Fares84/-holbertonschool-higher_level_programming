41216a4b6d081da0cabe5d96c8c085ebeae617f2
Fares84 <fares.sassi2015@gmail.com>
2020-12-16 12:20:17 +0000

    advanced args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    if len(argv) == 1:
         print("0 arguments.")
-    elif len(sys.argv) == 2:
-        print("1 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(len(argv)-1)
+        print("{} arguments:".format(len(argv)))
+        for i in range(1, len(argv)):
+            print("{} : {}".format(i, argv[i]))
