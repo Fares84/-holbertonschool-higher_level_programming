3c98d47e6724b2353ea3bbcef77ad0349117747d
Fares84 <fares.sassi2015@gmail.com>
2021-01-05 22:58:54 +0000

    updated files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Define a Square Class """
+
+
+class Square:
+    """ Class Square """
+
+    def __init__(self, size):
+        """ define square """
+
+        self.__size = size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Square Class """
+
+
+
+class Square:
+    """ class square """
+
+    def __init__(self, size=0):
+        """ class constructor """
+        self.size = size
+
+    def area(self):
+        """ method area """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ size def """
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be >= 0")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    
