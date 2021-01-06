7ea982a6adfe71e185ae66b21c51c183e37d4265
Fares84 <fares.sassi2015@gmail.com>
2021-01-06 22:01:26 +0000

    draw square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Square Class """
+
+
+class Square:
+    """ class square """
+
+    def __init__(self, size=0):
+        """ class constructor """
+        self.size = size
+
+    def area(self):
+        """ method area """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ size def """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be >= 0")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """ print square in # """
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
