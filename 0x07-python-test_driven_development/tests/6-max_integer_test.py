239be76a0b29a5f9e05d488bb6edd0a9847fe838
Fares84 <fares.sassi2015@gmail.com>
2021-01-15 17:58:58 +0000

    fixed unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
 """
-unit test for max_integer ([..])
-"""
-
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
@@ -14,25 +11,25 @@ class TestMaxInteger(unittest.TestCase):
 
     unittest {[builtin]} -- [built in to unit test]
     """
-    def test_if_empty_list(self):
+    def test_empty_list(self):
         """
         test if list is empty
         """
         self.assertEqual(max_integer([]), None)
 
-    def test_max_at_begin(self):
+    def test_max_begin(self):
         """
         test if max int is at the begining
         """
-        self.assertEqual(max_integer([5, 3, 4, 1]), 1)
+        self.assertEqual(max_integer([5, 3, 4, 1]), 5)
 
-    def test_max_at_mid(self):
+    def test_max_mid(self):
         """
         test if max int is at the middle
         """
         self.assertEqual(max_integer([5, 3, 8, 4, 1]), 8)
 
-    def test_max_at_end(self):
+    def test_max_end(self):
         """
         test if max int is at the end
         """
@@ -44,9 +41,8 @@ class TestMaxInteger(unittest.TestCase):
         """
         self.assertEqual(max_integer([-5, -3, -4, -8]), -3)
 
-    def test_loat_int(self)
-        """
-        test max through float and int
+    def test_float_int(self):
+        """ test max through float and int
         """
         self.assertEqual(max_integer([-5, 4.5, 4, 9.66]), 9.66)
 
@@ -54,13 +50,13 @@ class TestMaxInteger(unittest.TestCase):
         """
         test max through a string
         """
-        self.assertEqual(max_integer(["abcdef"]), f)
+        self.assertEqual(max_integer("abcdef"), "f")
 
     def test_mixed_int_str(self):
         """
         test mised types in list
         """
-        with self.assertRaises(Typerror):
+        with self.assertRaises(TypeError):
             max_integer(["hol", "berton", 7])
 
 if __name__ == '__main__':
