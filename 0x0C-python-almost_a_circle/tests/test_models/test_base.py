45fc0193f93fbf9b8bf6d67e99e1f7ca663879c6
Fares84 <fares.sassi2015@gmail.com>
2021-01-29 01:38:20 +0000

    updated file

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -17,30 +17,37 @@ class TestBase(unittest.TestCase):
         self.assertEqual(B.id, 1)
 
     def test_base_negative(self):
+        """ test negative """
         B = Base(-1)
         self.assertEqual(B.id, -1)
 
     def test_base_int(self):
+        """ test int """
         B = Base(53)
         self.assertEqual(B.id, 53)
 
     def test_base_string(self):
+        """ test str """
         B = Base("fares")
         self.assertEqual(B.id, "fares")
 
     def test_base_none(self):
+        """ test None """
         B = Base(None)
         self.assertEqual(B.id, 1)
 
     def test_base_float(self):
+        """ test float """
         B = Base(3.14)
         self.assertEqual(B.id, 3.14)
 
     def test_base_list(self):
+        """ test list """
         B = Base([1, 2, 3, 4])
         self.assertEqual(B.id, [1, 2, 3, 4])
 
     def test_base_dict(self):
+        """ test dict """
         B = Base({"Holberton": "School"})
         self.assertEqual(B.id, {"Holberton": "School"})
 
