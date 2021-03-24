fa1911ec63a61102e343ed1c9bed6c1d3f50c9d6
Fares84 <fares.sassi2015@gmail.com>
2021-03-24 16:52:50 +0000

    fixed file added sort and slice

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const _argument = process.argv;
+if (_argument.length <= 3) {
+  console.log(0);
+} else {
+  /* slice returns a shallow copy of a portion of an array
+  and indicates (-2) extracts the last two elements in the sequence */
+  /* sort accepts a comparative fuction which in this case
+  organizes values in ascendent order */
+  console.log(_argument.slice(2).sort((a, b) => a - b).reverse()[1]);
+}
