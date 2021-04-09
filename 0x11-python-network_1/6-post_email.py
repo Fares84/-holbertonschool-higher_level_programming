a6ee8505993d45403a4678b0d547d078ff7d7dac
Fares84 <fares.sassi2015@gmail.com>
2021-04-09 20:19:56 +0000

    fix pep8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL and an email address, sends a POST request to
-the passed URL with the email as a parameter, and finally displays the body of
-the response.
+""" Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
 The email must be sent in the variable email
 packages requests and sys """
 import requests
