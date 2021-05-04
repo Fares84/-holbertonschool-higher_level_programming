533fdf92731bfbdc83e42bed39ff1b705c7a9dbe
Fares84 <fares.sassi2015@gmail.com>
2021-05-04 21:29:03 +0000

    fixed file uppercase File

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const fileName = process.argv[2];
 const dataFile = process.argv[3];
 
-fs.writefile(fileName, dataFile, 'utf-8', (error) => {
+fs.writeFile(fileName, dataFile, 'utf-8', (error) => {
   if (error) {
     console.log(error);
   }
